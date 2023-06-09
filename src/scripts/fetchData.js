3b1a05c54e2878c3fad7cc3bd44bee65c2f1e325
richi-coder <richicoder@gmail.com>
2023-06-09 16:19:32 -0400

    better

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -2,7 +2,6 @@ export const fetchData = async() => {
     const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=20&query=painting&client_id=${import.meta.env.VITE_TEST_KEY}`);
     const data = await res.json();
     const results = data.results;
-    console.log(results, import.meta.env.MODE, import.meta.env.DEV, import.meta.env);
     return results;
 }
 
