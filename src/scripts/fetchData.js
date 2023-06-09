581c085a52529f0fdffdd97d1792f2ee514300f0
richi-coder <richicoder@gmail.com>
2023-06-09 17:26:55 -0400

    changing fetchData to fetch from my own API, instead of unsplash

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -1,8 +1,8 @@
 export const fetchData = async() => {
-    const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=20&query=painting&client_id=${import.meta.env.VITE_TEST_KEY}`);
+    const res = await fetch(`https://unsplash-api-richi-coder.vercel.app/api/3dgallery?query=paintings`);
     const data = await res.json();
-    const results = data.results;
-    return results;
+    // const results = data.results;
+    return data;
 }
 
 
