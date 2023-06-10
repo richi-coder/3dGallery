de87e1bc6309c5ab7e7134ed9659ab4e1dca5e9d
richi-coder <richicoder@gmail.com>
2023-06-10 19:11:44 -0400

    presentation animation, working, lacking wainting for building canvas

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react"
+import { UseAppContext } from "../context/AppContext";
+
 function Presentation() {
+    const useAppContext = UseAppContext();
+    const [presentation, setPresentation] = useState('z-[100] block')
+    const [presentationContent, setPresentationContent] = useState('opacity-0 translate-y-full')
+
+    useEffect(() => {
+     setPresentationContent('opacity-100 translate-y-0')
+    }, [])
+    
+    useEffect(() => {
+        setTimeout(() => {
+            if (useAppContext.state.data.length > 0) {
+                setPresentationContent('-translate-y-[100vh] opacity-0')
+                setTimeout(() => {
+                    setPresentation('-z-[100] hidden')
+                }, 1000);
+                
+            }
+        }, 2000);
+    }, [useAppContext.state.data[0]])
+
   return (
-    <div id="presentation" className="fixed top-0 left-0 w-screen h-screen bg-black text-white z-[100] flex flex-col items-center justify-center">
-        <div id="presentation-content" className="flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl w-fit h-fit">
+    <div id="presentation" className={`fixed top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center ${presentation}`}>
+        <div id="presentation-content" className={`flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl w-fit h-fit ${presentationContent} duration-1000`}>
             <div className="logo">
             3dGallery
             </div>
             <div>
                 |
             </div>
-            <h1 class="text-center p-0 m-0 richi-logo">
-                richi<span class="text-gradient">Coder</span>
+            <h1 className="text-center p-0 m-0 richi-logo">
+                richi<span className="text-gradient">Coder</span>
                 </h1>
         </div>
     </div>
