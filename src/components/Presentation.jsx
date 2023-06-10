af8f9fe836eb3bbb040fab1100d28aec3ee55429
richi-coder <richicoder@gmail.com>
2023-06-10 18:25:55 -0400

    lacking animation at first preesntation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,15 +1,17 @@
 function Presentation() {
   return (
-    <div id="presentation" className="fixed top-0 left-0 w-screen h-screen bg-black text-white z-[100] flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl">
-        <div className="logo">
-        3dGallery
+    <div id="presentation" className="fixed top-0 left-0 w-screen h-screen bg-black text-white z-[100] flex flex-col items-center justify-center">
+        <div id="presentation-content" className="flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl w-fit h-fit">
+            <div className="logo">
+            3dGallery
+            </div>
+            <div>
+                |
+            </div>
+            <h1 class="text-center p-0 m-0 richi-logo">
+                richi<span class="text-gradient">Coder</span>
+                </h1>
         </div>
-        <div>
-            |
-        </div>
-        <h1 class="text-center p-0 m-0 richi-logo">
-              richi<span class="text-gradient">Coder</span>
-            </h1>
     </div>
   )
 }
