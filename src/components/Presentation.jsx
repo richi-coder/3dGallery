9c4ee2cd60ab338d57e1f7c499ebdbdd48aa89e1
richi-coder <richicoder@gmail.com>
2023-06-10 19:14:48 -0400

    better animation, lacking loading bar

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,16 +4,13 @@ import { UseAppContext } from "../context/AppContext";
 function Presentation() {
     const useAppContext = UseAppContext();
     const [presentation, setPresentation] = useState('z-[100] block')
-    const [presentationContent, setPresentationContent] = useState('opacity-0 translate-y-full')
+    const [presentationContent, setPresentationContent] = useState('opacity-0 translate-y-[100%]')
 
     useEffect(() => {
-     setPresentationContent('opacity-100 translate-y-0')
-    }, [])
-    
-    useEffect(() => {
+        setPresentationContent('translate-y-0 opacity-100 ')
         setTimeout(() => {
             if (useAppContext.state.data.length > 0) {
-                setPresentationContent('-translate-y-[100vh] opacity-0')
+                setPresentationContent('-translate-y-[200%] opacity-0')
                 setTimeout(() => {
                     setPresentation('-z-[100] hidden')
                 }, 1000);
@@ -24,7 +21,7 @@ function Presentation() {
 
   return (
     <div id="presentation" className={`fixed top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center ${presentation}`}>
-        <div id="presentation-content" className={`flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl w-fit h-fit ${presentationContent} duration-1000`}>
+        <div id="presentation-content" className={`flex flex-row items-center justify-center gap-x-10 text-xl md:text-3xl w-fit h-fit ${presentationContent} transition-all duration-1000`}>
             <div className="logo">
             3dGallery
             </div>
